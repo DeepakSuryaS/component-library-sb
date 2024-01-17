e792177f39fcec257c0e1f2ce7573c1c5173d1b2
Deepak Surya <ideepaksuryas@gmail.com>
2024-01-17 20:06:23 +0530

    add: global decorator for SB

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,27 @@
 /** @type { import('@storybook/react').Preview } */
+import React from "react";
 import "minireset.css";
 import "../styles/global.css";
 import "../styles/tokens.css";
+
+const styles = {
+  display: "flex",
+  flexDirection: "column",
+  maxHeight: "auto",
+  justifyContent: "flex-start",
+  alignItems: "flex-start",
+  flexWrap: "wrap",
+  height: "100%",
+  gap: "10px 30px",
+};
+
+export const decorators = [
+  (Story) => (
+    <div style={styles}>
+      <Story />
+    </div>
+  ),
+];
 
 const preview = {
   parameters: {
